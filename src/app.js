24b9185fce6e55c43bb8e94f88b2462551cdf0ff
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2021-03-06 04:38:38 +0600

    update

--- src/app.js
+++ src/app.js
@@ -6,12 +6,15 @@
  
  const port=process.env.PORT || 3200;
 
+//  must include
  app.use(express.json());
 
  app.get("/",(req,res)=>{
      res.send("Hello from the default side")
  })
 
+
+//  post a data
  app.post('/students',(req,res)=>{
     console.log(req.body);
     const user=new Student(req.body);
@@ -23,6 +26,26 @@
     
  })
 
+
+
+//deleta single data
+app.delete('/students/:id',async(req,res)=>{
+    try{
+        const _id=req.params.id;
+        const studentsData= await Student.findByIdAndDelete(_id);
+        if(!studentsData){
+            return res.status(400).send()
+        }else{
+            res.send(studentsData)
+        }
+    }catch(e){
+        res.status(500).send(e);
+    }
+})
+
+
+
+// show all data --> GET Method
 app.get('/students',async(req,res)=>{
     try{
         const studentsData= await Student.find();
@@ -33,22 +56,30 @@ app.get('/students',async(req,res)=>{
 })
 
 
-
-app.get('/students',async(req,res)=>{
-    try{
-        const studentsData= await Student.find();
-        res.send(studentsData);
-    }catch(e){
-        res.send(e);
-    }
-})
-
-
+// find one particular data by ID
 // http://localhost:3200/students/604139cba1a8bb05407070e4
 app.get('/students/:id',async(req,res)=>{
     try{
         const _id=req.params.id;
         const studentsData= await Student.findById(_id);
+        if(!studentsData){
+            return res.status(404).send()
+        }else{
+            res.send(studentsData)
+        }
+    }catch(e){
+        res.status(500).send(e);
+    }
+})
+
+
+// update data
+app.patch('/students/:id',async(req,res)=>{
+    try{
+        const _id=req.params.id;
+        const studentsData= await Student.findByIdAndUpdate(_id,req.body,{
+            new:true
+        });
         if(!studentsData){
             return res.status(404).send()
         }else{
